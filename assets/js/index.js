ded0764a8c095e2a3e0348f23eecffca8904605a
Ryuki <ryuki.hayate.1520@gmail.com>
2018-08-19 22:23:33 +0900

    ローディング画面を追加、htmlファイルからjsを分離

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+$(function(){
+  $('#jsi-btn-hamburger').click(function() {
+    $(this).toggleClass("active");
+      if ($(this).hasClass('active')) {
+        $('.nav-menu').addClass('active');
+      } else {
+        $('.nav-menu').removeClass('active');
+      }
+  });
+});
+
+$(function(){
+ $(window).on('load',function(){
+   $("#pageloading-wrap").delay(1000).fadeOut('slow');
+ });
+ function loaderClose(){
+   $("#pageloading-wrap").fadeOut('slow');
+ }
+ setTimeout(loaderClose,5000);
+});
